518f2667faf511c545bec714540c40487e5e4f78
Riyaz <sr308379@gmail.com>
2025-05-14 22:06:03 +0530

    feat: enable secure cookies for enhanced security in Google authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,7 +97,7 @@ const googleAuth = async (req, res) => {
     // Set cookie with token
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000,
     });
