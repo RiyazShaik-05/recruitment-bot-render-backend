f779604f0c6da9396b07c2a08dcad74c44dbcc99
Riyaz <sr308379@gmail.com>
2025-05-14 22:16:58 +0530

    feat: update CORS origin to use specific production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 const allowedOrigin = process.env.CLIENT_URI;
 app.use(
   cors({
-    origin: allowedOrigin,
+    origin: "https://recruitment-bot-vercel-6f2z.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
