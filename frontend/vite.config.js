85d775b4de7252608ffa550d3f286ab481dda080
Riyaz <sr308379@gmail.com>
2025-05-15 10:18:42 +0530

    feat: update backend .gitignore and modify Vite proxy target for API requests

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:9876',
+        target: 'https://recruitment-bot-vercel-backend-i1lljia9z-riyaz-shaiks-projects.vercel.app',
         changeOrigin: true,
         secure: true,
       },
